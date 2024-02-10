e97e0fc7f52a55ae34c49df3e0f6ecbbba215ab1
mksah98 <mintu.sah@ximkart.com>
2024-02-10 17:49:55 +0530

    fix: :bug: formatting and removed deprecated deps

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,22 @@
-import Head from 'next/head'
-import { BsFillMoonStarsFill } from 'react-icons/bs';
-import { AiFillGithub, AiFillLinkedin, AiFillMail, AiOutlineReload } from 'react-icons/ai';
-import Link from 'next/link';
+import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
+import Link from "next/link";
 import { FcHome } from "react-icons/fc";
 
-import Image from 'next/image';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import Image from "next/image";
+import { useState } from "react";
 
-import devAvatar from '../public/devAvatar.png';
-import TechnologyUsed from '../components/TechnologyUsed';
+import devAvatar from "../public/devAvatar.png";
+import TechnologyUsed from "../components/TechnologyUsed";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
-  const [quoteData, setQuoteData] = useState({});
-  const linkedinUrl = "https://www.linkedin.com/in/mintu-kumar-sah-24914417b/";
+  const linkedinUrl = "https://www.linkedin.com/in/mintuksah/";
   const githubUrl = "https://github.com/mksah98";
   const [activeLinkedin, setActiveLinkedin] = useState("");
   const [activeGithub, setActiveGithub] = useState("");
   const [activeMail, setActiveMail] = useState("");
-
-  useEffect(() => {
-    fetchRandomQuote();
-  }, [])
-
-  const fetchRandomQuote = async () => {
-
-    const response = await axios("https://type.fit/api/quotes");
-    const data = response.data;
-    const randomNumber = Math.floor(Math.random() * 1643);
-
-    setQuoteData(data[randomNumber]);
-  };
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -40,113 +25,169 @@ export default function Home() {
         <meta name="description" content="Portfolio Website" />
         <link rel="icon" href="/favicon-n.ico" type="image/x-icon" />
       </Head>
-      <main className='bg-white px-10  dark:bg-gray-800'>
-        <section className='min-h-screen'>
-          <nav className='py-10 mb-1 flex justify-between'>
-            <h1 className='text-xl font-burtons dark:text-white'>developedbyMKS</h1>
-            <ul className='flex items-center gap-6'>
+      <main className="bg-white px-10  dark:bg-gray-800">
+        <section>
+          <nav className="py-10 mb-1 flex justify-between">
+            <h1 className="text-xl font-burtons dark:text-white">
+              developedbyMKS
+            </h1>
+            <ul className="flex items-center gap-6">
               <li>
                 <Link href="/">
-                  <FcHome className='cursor-pointer' />
+                  <FcHome className="cursor-pointer" />
                 </Link>
               </li>
               <li>
-                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer' color={darkMode ? 'white' : ""} />
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer"
+                  color={darkMode ? "white" : ""}
+                />
               </li>
               <li>
-                <a className='bg-gradient-to-r from-cyan-500 to-teal-300 text-white px-4 py-2 rounded-md ' href='https://drive.google.com/file/d/1N8PbLJ2LGDEDPQSIYGaloRE1BO5nTi9u/view' rel="noreferrer noopener" target="_blank">Resume</a>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-300 text-white px-4 py-2 rounded-md "
+                  href="https://drive.google.com/file/d/1N8PbLJ2LGDEDPQSIYGaloRE1BO5nTi9u/view"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
-          <div className='text-center'>
-            <h2 className='text-5xl py-2 mb-2 text-teal-500 font-medium'>Hi, I'm Mintu👋</h2>
-            <h2 className='text-2xl font-burtons dark:text-white'>Software Engineer </h2>
-            <p className='text-xl py-3 font-burtons leading-8 text-gray-800 sm:text-md dark:text-white'>
+          <div className="text-center">
+            <h2 className="text-5xl py-2 mb-2 text-teal-500 font-medium">
+              Hi, I'm Mintu👋
+            </h2>
+            <h2 className="text-2xl font-burtons dark:text-white">
+              Software Engineer{" "}
+            </h2>
+            <p className="text-xl py-3 font-burtons leading-8 text-gray-800 sm:text-md dark:text-white">
               Delivering complex products in less time with high quality
             </p>
           </div>
-          <div className='relative w-80 h-80 mx-auto'>
-            <Image className="rounded-full" src={devAvatar} layout='fill' alt="DevAvatar" />
+          <div className="relative w-80 h-80 mx-auto">
+            <Image
+              className="rounded-full"
+              src={devAvatar}
+              layout="fill"
+              alt="DevAvatar"
+            />
           </div>
-          <div className='text-5xl flex justify-center py-10 gap-16 text-gray-600'>
-            <AiFillLinkedin title="LinkedIn Profile" className="cursor-pointer dark:text-white" onMouseOver={() => setActiveLinkedin('blue')} onMouseLeave={() => setActiveLinkedin("")} color={activeLinkedin ? activeLinkedin : ""} onClick={() => window.open(linkedinUrl)} />
-            <AiFillGithub title="Github Profile" className="cursor-pointer dark:text-white" onMouseOver={() => setActiveGithub('black')} onMouseLeave={() => setActiveGithub("")} color={activeGithub ? activeGithub : ""} onClick={() => window.open(githubUrl)} />
-            <AiFillMail title="Mail" className="cursor-pointer dark:text-white" onMouseOver={() => setActiveMail('teal')} onMouseLeave={() => setActiveMail("")} color={activeMail ? activeMail : ""} onClick={() => window.open("mailto:krmintusah@gmail.com")} />
+          <div className="text-5xl flex justify-center py-10 gap-16 text-gray-600">
+            <AiFillLinkedin
+              title="LinkedIn Profile"
+              className="cursor-pointer dark:text-white"
+              onMouseOver={() => setActiveLinkedin("blue")}
+              onMouseLeave={() => setActiveLinkedin("")}
+              color={activeLinkedin ? activeLinkedin : ""}
+              onClick={() => window.open(linkedinUrl)}
+            />
+            <AiFillGithub
+              title="Github Profile"
+              className="cursor-pointer dark:text-white"
+              onMouseOver={() => setActiveGithub("black")}
+              onMouseLeave={() => setActiveGithub("")}
+              color={activeGithub ? activeGithub : ""}
+              onClick={() => window.open(githubUrl)}
+            />
+            <AiFillMail
+              title="Mail"
+              className="cursor-pointer dark:text-white"
+              onMouseOver={() => setActiveMail("teal")}
+              onMouseLeave={() => setActiveMail("")}
+              color={activeMail ? activeMail : ""}
+              onClick={() => window.open("mailto:krmintusah@gmail.com")}
+            />
           </div>
         </section>
         <TechnologyUsed />
         <section>
           <div>
-            <h3 className='text-3xl py-1 font-burtons dark:text-gray-200'>What I Provide</h3>
-            <div className='text-xl py-2 leading-8 text-gray-800 dark:text-gray-200'>
-              <h3>- Design, Development, and Maintenance of Web and Mobile Applications.</h3>
+            <h3 className="text-3xl py-1 font-burtons dark:text-gray-200">
+              What I Provide
+            </h3>
+            <div className="text-xl py-2 leading-8 text-gray-800 dark:text-gray-200">
+              <h3>
+                - Design, Development, and Maintenance of Web and Mobile
+                Applications.
+              </h3>
               <h3>- Highly interactive Front end / User Interfaces.</h3>
               <h3>- Building AWS Lambda Functions.</h3>
               <h3>- DataFix Support.</h3>
             </div>
           </div>
         </section>
-        <div className='md:m-10 p-20 flex-col border-spacing-4 shadow-lg dark:bg-slate-200'>
-          <div className='grid justify-items-end p-2'>
-            <AiOutlineReload onClick={() => fetchRandomQuote()} />
-          </div>
-          <h2 className='md:text-5xl sm:text-4xl font-burtons text-yellow-600 text-center md:px-10'>
-            "{quoteData.text} "
-          </h2>
-          <p className='md:text-xl text-center px-20 m-2'>
-            {quoteData.author}
-          </p>
-        </div>
-        <section className=' mt-4'>
-          <h3 className='text-3xl py-1 font-burtons dark:text-gray-200'>Projects</h3>
-          <div className='lg:flex gap-10'>
-            <div className='basis-1/2 text-center shadow-lg p-10 rounded-xl my-5 bg-slate-100 '>
-              <h3 className='text-xl font-bold px-2'>Basic Projects</h3>
-              <p className='py-1 flex-wrap'>
-                Calculator, Find An Activity
-              </p>
+        <section className=" mt-4">
+          <h3 className="text-3xl py-1 font-burtons dark:text-gray-200">
+            Projects
+          </h3>
+          <div className="lg:flex gap-10">
+            <div className="basis-1/2 text-center shadow-lg p-10 rounded-xl my-5 bg-slate-100 ">
+              <h3 className="text-xl font-bold px-2">Basic Projects</h3>
+              <p className="py-1 flex-wrap">Calculator, Find An Activity</p>
               <Link href="/levelB">
-                <div className='p-2 shadow-md mt-3 hover:bg-teal-300 font-burtons cursor-pointer'>
+                <div className="p-2 shadow-md mt-3 hover:bg-teal-300 font-burtons cursor-pointer">
                   View Projects
                 </div>
               </Link>
             </div>
-            <div className=' basis-1/2 text-center shadow-lg p-10 rounded-xl my-5 bg-slate-100 '>
-              <h3 className='text-xl font-bold px-2'>Web Applications Projects</h3>
-              <p className='py-1 flex-wrap'>
-                My Portfolio
-              </p>
+            <div className=" basis-1/2 text-center shadow-lg p-10 rounded-xl my-5 bg-slate-100 ">
+              <h3 className="text-xl font-bold px-2">
+                Web Applications Projects
+              </h3>
+              <p className="py-1 flex-wrap">My Portfolio</p>
               <Link href="/levelA">
-                <div className='p-2 shadow-md hover:bg-teal-300 mt-3 font-burtons cursor-pointer'>
+                <div className="p-2 shadow-md hover:bg-teal-300 mt-3 font-burtons cursor-pointer">
                   View Projects
                 </div>
               </Link>
             </div>
           </div>
         </section>
-        <section className='border-spacing-4 shadow-lg font-burtons p-10 m-5 rounded-xl dark:bg-slate-200'>
-          <h3 className='text-3xl py-1 mb-3 '>About me!</h3>
-          <div className='flex flex-wrap gap-4 justify-around md:p-5 bg-slate-100 rounded-lg shadow-lg '>
-            <p className='md:text-xl md:p-5 text-center w-80 text-cyan-500'>
-              `I am a dedicated and diligent individual toward the world of computers.
-              having a self driven attitude to embrace new tools and skills.
-              I have niche towards web-development. Living life, having fun, enjoying the sunset & nature.`
+        <section className="border-spacing-4 shadow-lg font-burtons p-10 m-5 rounded-xl dark:bg-slate-200">
+          <h3 className="text-3xl py-1 mb-3 ">About me!</h3>
+          <div className="flex flex-wrap gap-4 justify-around md:p-5 bg-slate-100 rounded-lg shadow-lg ">
+            <p className="md:text-xl md:p-5 text-center w-80 text-cyan-500">
+              `I am a dedicated and diligent individual toward the world of
+              computers. having a self driven attitude to embrace new tools and
+              skills. I have niche towards web-development. Living life, having
+              fun, enjoying the sunset & nature.`
             </p>
-            <img className="w-1/4 rounded-full border-4 " src={'https://user-images.githubusercontent.com/93177298/199574857-d98d393d-44d1-472c-91ee-0ad8cfb6026f.png'}>
-            </img>
+            <img
+              className="w-1/4 rounded-full border-4 "
+              src={
+                "https://user-images.githubusercontent.com/93177298/199574857-d98d393d-44d1-472c-91ee-0ad8cfb6026f.png"
+              }
+            ></img>
           </div>
         </section>
-        <div className='border-spacing-4 shadow-lg p-10 m-5 rounded-xl font-burtons dark:bg-slate-200'>
-          <h3 className='text-3xl py-1 mb-3 '>Reach Out to me!</h3>
-          <div className='font-burtons p-5 md:text-12md flex-col '>
-            <div className='flex items-center flex-wrap gap-12'>
-              <h3 className='m-4' > <span className='text-red-500 text-2xl'>Software Engineer</span> @BYJU's <span className='text-black-500 text-2xl'>💭</span></h3>
-              <h3 className='m-4'>Open for opportunities: <span className='text-red-500 text-2xl'>Yes</span></h3>
-              <h3 className='m-4'>Mail: <span className='text-red-500 md:text-2xl'><a>krmintusah@gmail.com</a></span></h3>
+        <div className="border-spacing-4 shadow-lg p-10 m-5 rounded-xl font-burtons dark:bg-slate-200">
+          <h3 className="text-3xl py-1 mb-3 ">Reach Out to me!</h3>
+          <div className="font-burtons p-5 md:text-12md flex-col ">
+            <div className="flex items-center flex-wrap gap-12">
+              <h3 className="m-4">
+                {" "}
+                <span className="text-red-500 text-2xl">
+                  Software Engineer
+                </span>{" "}
+                @ Ximkart <span className="text-black-500 text-2xl">💭</span>
+              </h3>
+              <h3 className="m-4">
+                Open for opportunities:{" "}
+                <span className="text-red-500 text-2xl">Yes</span>
+              </h3>
+              <h3 className="m-4">
+                Mail:{" "}
+                <span className="text-red-500 md:text-2xl">
+                  <a>krmintusah@gmail.com</a>
+                </span>
+              </h3>
             </div>
           </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
--- pages/levelA/index.js
+++ pages/levelA/index.js
@@ -1,11 +1,9 @@
-
-import { BsFillMoonStarsFill } from 'react-icons/bs';
-import { useState } from 'react';
-import Link from 'next/link';
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useState } from "react";
+import Link from "next/link";
 import { FcHome } from "react-icons/fc";
 
 const LandingPage = () => {
-
   const [darkMode, setDarkMode] = useState(false);
   const [showComponent, setShowComponent] = useState("");
 
@@ -17,7 +15,8 @@ const LandingPage = () => {
 
     switch (projectName) {
       case "portfolio":
-        portfolioImage = "https://user-images.githubusercontent.com/93177298/199567034-f2a809bb-1146-4401-b25e-68172bc6217d.png";
+        portfolioImage =
+          "https://user-images.githubusercontent.com/93177298/199567034-f2a809bb-1146-4401-b25e-68172bc6217d.png";
         portfolioUrl = "https://my-portfolio-mksah98.vercel.app/";
         portfolioGithubUrl = "https://github.com/mksah98/myPortfolio.github.io";
         description = `A Portfolio Website to showcase about the project and technology I used and worked on.
@@ -28,59 +27,94 @@ const LandingPage = () => {
         break;
     }
     return (
-      <div className='flex-col m-5 justify-center items-center'>
-        <div className='flex gap-4 justify-around'>
-          <div className=' basis-1/2'>
+      <div className="flex-col m-5 justify-center items-center">
+        <div className="flex gap-4 justify-around">
+          <div className=" basis-1/2">
             <img src={portfolioImage} className="object-cover" />
           </div>
-          <div className=' basis-1/2 items-center'>
-            <p className='text-center p-2 font-burtons text-xl'>
+          <div className=" basis-1/2 items-center">
+            <p className="text-center p-2 font-burtons text-xl">
               {description}
             </p>
           </div>
         </div>
-        <div className='flex gap-4 justify-center mt-3'>
-          {projectName !== 'movieApp' ? <button onClick={() => window.open(portfolioUrl)} className=' shadow-lg px-4 text-center py-1 text-[1.25rem] font-burtons  bg-cyan-500 dark:text-white rounded-lg'>Preview</button> : ''}
-          <button onClick={() => window.open(portfolioGithubUrl)} className='px-4 text-center py-1 text-[1.25rem] drop-shadow-lg font-burtons  bg-cyan-500 rounded-lg dark:text-white'>Code</button>
+        <div className="flex gap-4 justify-center mt-3">
+          {projectName !== "movieApp" ? (
+            <button
+              onClick={() => window.open(portfolioUrl)}
+              className=" shadow-lg px-4 text-center py-1 text-[1.25rem] font-burtons  bg-cyan-500 dark:text-white rounded-lg"
+            >
+              Preview
+            </button>
+          ) : (
+            ""
+          )}
+          <button
+            onClick={() => window.open(portfolioGithubUrl)}
+            className="px-4 text-center py-1 text-[1.25rem] drop-shadow-lg font-burtons  bg-cyan-500 rounded-lg dark:text-white"
+          >
+            Code
+          </button>
         </div>
       </div>
-    )
+    );
   };
 
-  return (<div className={darkMode ? "dark" : ""}>
-    <div className='bg-white px-10 h-screen w-screen dark:bg-gray-800'>
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <div className="bg-white px-10 h-screen w-screen dark:bg-gray-800">
+        <section>
+          <nav className="py-10 mb-1 flex justify-between">
+            <h1 className="text-xl font-burtons dark:text-white">
+              developedbyMKS
+            </h1>
+            <ul className="flex items-center gap-6">
+              <li>
+                <Link href="/">
+                  <FcHome className="cursor-pointer" />
+                </Link>
+              </li>
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className="cursor-pointer"
+                  color={darkMode ? "white" : ""}
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-300 text-white px-4 py-2 rounded-md ml-8"
+                  href="https://drive.google.com/file/d/1jqkeK_j7QOBdkJW97uUeNJPo67qmMO_o/view?usp=sharing"
+                  rel="noreferrer noopener"
+                  target="_blank"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </section>
 
-      <section>
-        <nav className='py-10 mb-1 flex justify-between'>
-          <h1 className='text-xl font-burtons dark:text-white'>developedbyMKS</h1>
-          <ul className='flex items-center gap-6'>
-            <li>
-              <Link href="/">
-                <FcHome className='cursor-pointer' />
-              </Link>
-            </li>
-            <li>
-              <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer' color={darkMode ? 'white' : ""} />
-            </li>
-            <li>
-              <a className='bg-gradient-to-r from-cyan-500 to-teal-300 text-white px-4 py-2 rounded-md ml-8' href='https://drive.google.com/file/d/1jqkeK_j7QOBdkJW97uUeNJPo67qmMO_o/view?usp=sharing' rel="noreferrer noopener" target="_blank">Resume</a>
-            </li>
-          </ul>
-        </nav>
-      </section>
+        <section>
+          <h2 className="text-4xl mb-5 font-burtons dark:text-white">
+            Projects
+          </h2>
+          <div className="flex flex-wrap  m-5 gap-10">
+            <div
+              className="shadow-lg rounded-md bg-white drop-shadow-lg p-3 font-bold hover:bg-sky-200"
+              onClick={() => setShowComponent("portfolio")}
+            >
+              Portfolio
+            </div>
+          </div>
+        </section>
 
-      <section>
-        <h2 className='text-4xl mb-5 font-burtons dark:text-white'>Projects</h2>
-        <div className='flex flex-wrap  m-5 gap-10'>
-          <div className='shadow-lg rounded-md bg-white drop-shadow-lg p-3 font-bold hover:bg-sky-200' onClick={() => setShowComponent('portfolio')}>Portfolio</div>
-        </div>
-      </section>
-
-      <section className='flex justify-center md:p-10 h-max'>
-        {showComponent && renderProject(showComponent)}
-      </section>
+        <section className="flex justify-center md:p-10 h-max">
+          {showComponent && renderProject(showComponent)}
+        </section>
+      </div>
     </div>
-  </div>)
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
